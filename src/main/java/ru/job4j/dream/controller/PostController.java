f58e864438d0bf1f80eb8427a6bc3c95ea62493b
Denis Davydov <dvd9@yandex.ru>
2022-03-25 21:10:13 +0300

    4. Thymeleaf, Циклы. [#504841]

--- src/main/java/ru/job4j/dream/controller/PostController.java
+++ src/main/java/ru/job4j/dream/controller/PostController.java
@@ -3,7 +3,6 @@ package ru.job4j.dream.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.PostStore;
 
 @Controller
